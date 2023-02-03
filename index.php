954d138ce4c3c62ca424ea7e90efef0f53cde2e0
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-02-03 12:07:28 +0100

    Updating index file

--- index.php
+++ index.php
@@ -115,15 +115,11 @@
                     <div class="modal-dialog">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Voulez-Vous Supprimer Cette Annonce ?</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
-                        Suppression
-                        </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button type="button" class="btn btn-danger">Confirmer</button>
                         </div>
                         </div>
                     </div>
