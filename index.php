b84ca1e69a0a6d684958ee49a7e2e1579b1cc4de
FatimaEzzahraAy <110188783+FatimaEzzahraAy@users.noreply.github.com>
2023-02-03 09:58:17 +0000

    t

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
     <body>
+
         <header class="container-fluid bg-dark">
             <div class="container">
                 <nav class="navbar navbar-expand-lg navbar-dark py-2">
@@ -87,6 +88,8 @@
                                 <p class="card-text">Publié le 01/02/2023.</p>
                                 <a href="#" class="btn btn-success">Modifier</a>
                                 <a href="#" class="btn btn-danger">Supprimer</a>    
+                                <a href="#" class="btn btn-danger">Supprimer</a>    
+
                             </div>
                         </div>
                     </div>
