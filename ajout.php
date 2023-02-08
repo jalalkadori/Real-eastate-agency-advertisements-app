db51d6cc0e624347663ec20a842941baad4cbc23
FatimaEzzahraAy <aydilih.f.e@gmail.com>
2023-02-08 16:53:45 +0100

    ajouterAvecSucces

--- ajout.php
+++ ajout.php
@@ -37,14 +37,14 @@
                 <h2 class="py-3 text-center">Ajouter une nouvelle annonce !</h2>
                 <div class="row d-flex justify-content-center">
                     <div class="col-6 border">
-                        <form action='index.php' method='POST'>
+                        <form action='' method='POST' enctype="multipart/form-data">
                             <div class='mb-3'>
                                 <label for='titre' class='form-label'>Titre d'annonce :</label>
                                 <input type='text' class='form-control' id='titre' name='titre'>
                             </div>
                             <div class='mb-3'>
                                 <label for="formFile" class="form-label">Image</label>
-                                <input class="form-control" type="file" id="formFile">
+                                <input class="form-control"  type="file" id="image" name="image">
                             </div>
                             <div class='mb-3'>
                                 <label for='Description' class='form-label'>Description : </label>
@@ -57,11 +57,11 @@
                             <div class='row mb-3'>
                                 <div class='col'>
                                     <label for='Superficie' class='form-label'>Superficie : </label>
-                                    <input type='number' class='form-control' id='Superficie' name='Superficie'>
+                                    <input type='number' class='form-control' id='Superficie' name='superficie'>
                                 </div>
                                 <div class='col'>
                                     <label for='Montant' class='form-label'>Montant : </label>
-                                    <input type='number' class='form-control' id='Montant' name='Montant'>
+                                    <input type='number' class='form-control' id='Montant' name='montant'>
                                 </div>
                                 <div class='col'>
                                     <label for='Adresse' class='form-label'>Date : </label>
@@ -71,12 +71,13 @@
                             <div class='mb-3'>
                                 <label for='Type' class='form-label'>Type d'annonce : </label>
                                 <select class='form-select' aria-label='type' id='Type' name='type'>
+                                    <option value="Selectionnez un choix">Selectionnez un choix</option>
                                     <option value='Location'>Location</option>
                                     <option value='Vente'>Vente</option>
                                 </select>
                             </div>
                             <div class='mb-3'>
-                                <button class="btn btn-dark w-100">Ajouter</button>
+                                <button class="btn btn-dark w-100" type="submit" name="ajouter">Ajouter</button>
                             </div>
                         </form>
                     </div>
@@ -85,7 +86,40 @@
             </section>
         </main>
            
+<?php 
+ if(isset($_POST['ajouter'])){
+    $titre =$_POST["titre"];
+    $desc = $_POST["description"];
+    $sup =$_POST["superficie"];
+    $add = $_POST["adresse"];
+    $montant = $_POST["montant"];
+    $date = $_POST["date"];
+    $type = $_POST["type"];
 
+    //image
+    $img = $_FILES["image"]["name"];
+    $fileExtension = explode('.', $img);     
+    $fileExtension = end($fileExtension);     
+    $allowedExtensions = array('jpg', 'png', 'jpeg','jfif');
+    $img = uniqid('', true). ".$fileExtension"; 
+    $tempname = $_FILES['image']['tmp_name'];
+    $folder = "./img/" . $img;
+    move_uploaded_file($tempname, $folder);
+
+    if(!empty($titre && $img && $desc && $sup && $add && $montant && $date && $type))
+    { 
+        if(strlen($titre) < 200 && strlen($desc) > 3  && strlen($add) > 3 && $sup > 0 && $montant > 0){//vverifier
+            echo "L'ajout avec succées";
+            $requeteAjout = "INSERT INTO `annonce` (`TitreAnnonce`, `ImageAnnonce`, `DescriptionAnnonce`, `SuperficieAnnonce`, `AdresseAnnonce`, `MontantAnnonce`, `DateAnnonce`, `TypeAnnonce`) VALUES ('$titre', '$img', '$desc', '$sup', '$add','$montant', '$date', '$type')";
+            $dbco->exec($requeteAjout);   
+        }
+    }
+  
+    //
+    
+}
+
+?>
         
 
 
--- connexion.php
+++ connexion.php
@@ -1,11 +1,10 @@
 <?php 
             try{
                 $dbco = new PDO("mysql:host=127.0.0.1;dbname=gestion_immobilier;charset=utf8mb4;", 'root', '');
-                
                 $requeteS ="SELECT IdAnnonce,ImageAnnonce,TitreAnnonce,TypeAnnonce,SuperficieAnnonce,MontantAnnonce,AdresseAnnonce,DateAnnonce FROM annonce";
                 $rest = $dbco->prepare($requeteS);
                 $rest->execute();
-                $count = $rest->rowCount(); //nbre de ligne d'une requete
+                 $count = $rest->rowCount(); //nbre de ligne d'une requete
 
                
             }
