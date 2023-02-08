b8f78ffe50be7227b8879e58fd04011bb638ef79
Jalal KADDOURI <jalalkadori2@gmail.com>
2023-02-08 14:51:56 +0100

    update index file

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                                     <a class="nav-link" href="#Annonce">Annonces</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a tupe"button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#AjoutModal"> + Ajouter une Annonce</a>
+                                    <a href="ajout.php" type"button" class="btn btn-outline-light"> + Ajouter une Annonce</a>
                                 </li>
                             </ul>
                         </div>
@@ -47,6 +47,7 @@
                     <div class="col">
                         <h5 for="type">Categorie</h5>
                         <select class="form-select" aria-label="type">
+                            <option value=""></option>
                             <option value="Location">Location</option>
                             <option value="Vente">Vente</option>
                         </select>
@@ -84,8 +85,8 @@
                                             <p class='card-text'>".$ligne["AdresseAnnonce"]."</p>
                                             <p class='card-text'>Publié le ".$ligne["DateAnnonce"].".</p>
                                             <form action='' method='POST'>
-                                            <button class='modifierCarteId btn btn-success' data-bs-toggle='modal' data-bs-target='#ModificationModal' name='modofierCarte' value=". $ligne['IdAnnonce'].">Modifier</button>
-                                            <button class='supprimerCarteId btn btn-danger' data-bs-toggle='modal' data-bs-target='#SuppressionModal' name='supprimerCarte' value=". $ligne['IdAnnonce'].">Supprimer</button> 
+                                            <a href='modification.php' class='modifierCarteId btn btn-success'>Modifier</a>
+                                            <a href='suppression.php' class='supprimerCarteId btn btn-danger'>Supprimer</a> 
                                             </form>
                                         </div>
                                     </div>
@@ -95,192 +96,7 @@
                     }
             echo "</div>";
     ?>
-                <!-- Ajout Modal -->
-                <div class='modal fade' id='AjoutModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                    <div class='modal-dialog'>
-                        <div class='modal-content'>
-                        <div class='modal-header'>
-                            <h1 class='modal-title fs-5' id='exampleModalLabel'>Ajouter une nouvelle annonce</h1>
-                            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                        </div>
-                        <div class='modal-body'>
-                            <form action='' method='POST'>
-                                <div class='mb-3'>
-                                    <label for='titre' class='form-label'>Titre d'annonce :</label>
-                                    <input type='file' class='form-control' id='titre' name='titre1'>
-                                </div>
-                                <div class='mb-3'>
-                                    <label for='image' class='form-label'>Chemin d'image: </label>
-                                    <input type='text' class='form-control' id='image' name='image'>
-                                </div>
-                                <div class='mb-3'>
-                                    <label for='Description' class='form-label'>Description : </label>
-                                    <textarea type='text' class='form-control' id='Description' name='description'></textarea>
-                                </div>
-                                <div class='mb-3'>
-                                    <label for='Adresse' class='form-label'>Adresse : </label>
-                                    <input type='text' class='form-control' id='Adresse' name='adresse'>
-                                </div>
-                                <div class='row mb-3'>
-                                    <div class='col'>
-                                        <label for='Superficie' class='form-label'>Superficie : </label>
-                                        <input type='number' class='form-control' id='Superficie' name='superficie'>
-                                    </div>
-                                    <div class='col'>
-                                        <label for='Montant' class='form-label'>Montant : </label>
-                                        <input type='number' class='form-control' id='Montant' name='montant'>
-                                    </div>
-                                    <div class='col'>
-                                        <label for='date' class='form-label'>Date : </label>
-                                        <input type='date' class='form-control' id='date' name='date'>
-                                    </div>
-                                </div>
-                                <div class='mb-3'>
-                                    <label for='Type' class='form-label'>Type d'annonce : </label>
-                                    <select class='form-select' aria-label='type' id='Type' name='type'>
-                                        <option value='Location'>Location</option>
-                                        <option value='Vente'>Vente</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class='modal-footer'>
-                                <button type='submit' class='btn btn-dark w-100' name='ajouter'>Ajouter</button>
-                            </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    <?php 
-                    if(isset($_POST['ajouter'])){//lorsque je clik sur le bouttant ajouter
-                        $titre =$_POST["titre1"];
-                        $img = $_POST["image"];
-                        $desc = $_POST["description"];
-                        $sup =$_POST["superficie"];
-                        $add = $_POST["adresse"];
-                        $montant = $_POST["montant"];
-                        $date = $_POST["date"];
-                        $type = $_POST["type"];
-
-                        $requeteAjout = "INSERT INTO `annonce` (`TitreAnnonce`, `ImageAnnonce`, `DescriptionAnnonce`, `SuperficieAnnonce`, `AdresseAnnonce`, `MontantAnnonce`, `DateAnnonce`, `TypeAnnonce`) VALUES ('$titre', '$img', '$desc', '$sup', '$add','$montant', '$date', '$type')"; // il faut qelle soit ici et pas dans l'autre fichier
-                        $dbco->exec($requeteAjout);
-                        
-                    }
-                    ?>
-
-                <!-- Modification Modal -->
-                <?php ///afficher les donnees dan mondal modifier
-            //    if(isset($_post['modofierCarte'])){//fonctionne pas
-            //     $IdMod =$_POST['indexMd'];
-            //     echo $IdMod;
-            //     $requeteMct = "SELECT * FROM annonce WHERE IdAnnonce=$IdMod";
-            //     $resultatRMCt = $dbco ->query($requeteMct);
-            //     $resultatRMCt->exec();
-            //     if($resultatRMCt){
-            //          $ligne = $resultatRMCt ->fetch(PDO::FETCH_ASSOC);
-            //          $_POST['titreM']=$ligne['TitreAnnonce'];
-            //         }
-            //     }
-                ?>
-           
-                <div class='modal fade' id='ModificationModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                    <div class='modal-dialog'>
-                        <div class='modal-content'>
-                            <div class='modal-header'>
-                                <h1 class='modal-title fs-5' id='exampleModalLabel'>Modification de l'annonce</h1>
-                                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                            </div>
-                        
-                            <form action='index.php' method='POST'>
-                                <div class='modal-body'>
-                                    <div class='mb-3'>
-                                        <label for='titre' class='form-label'>Titre d'annonce :</label>
-                                        <input type='text' class='form-control' id='titre' name='titreM' value=''>
-                                    </div>
-                                    <div class='mb-3'>
-                                        <label for='image' class='form-label'>Chemin d'image: </label>
-                                        <input type='text'  class='form-control' id='image' name='imageM'>
-                                    </div>
-                                    <div class='mb-3'>
-                                        <label for='Description' class='form-label'>Description : </label>
-                                        <textarea type='text' class='form-control' id='Description' name='descriptionM'></textarea>
-                                    </div>
-                                    <div class='mb-3'>
-                                        <label for='Adresse' class='form-label'>Adresse : </label>
-                                        <input type='text' class='form-control' id='Adresse' name='adresseM'>
-                                    </div>
-                                    <div class='row mb-3'>
-                                        <div class='col'>
-                                            <label for='Superficie' class='form-label'>Superficie : </label>
-                                            <input type='number' class='form-control' id='Superficie' name='superficieM'>
-                                        </div>
-                                        <div class='col'>
-                                            <label for='Montant' class='form-label'>Montant : </label>
-                                            <input type='number' class='form-control' id='Montant' name='montantM'>
-                                        </div>
-                                        <div class='col'>
-                                            <label for='Adresse' class='form-label'>Date : </label>
-                                            <input type='date' class='form-control' id='Date' name='dateM'>
-                                        </div>
-                                    </div>
-                                    <div class='mb-3'>
-                                        <label for='Type' class='form-label'>Type d'annonce : </label>
-                                        <select class='form-select' aria-label='type' id='Type' name='typeM'>
-                                            <option value='Location'>Location</option>
-                                            <option value='Vente'>Vente</option>
-                                        </select>
-                                        <input id='idinpMd'  name='indexMd' value=''></input>
-                                    </div>
-                                </div>
-                                <div class='modal-footer'>
-                                    <button type='submit' class='btn btn-success' name='modifier'>Modifier</button>
-                                </div>
-                            </form> 
-                        </div>  
-                    </div>
-                </div>   
-                    
-                    <?php
-                           $titreM = $_POST['titreM'];
-                           $imgM = $_POST['imageM'];
-                           $descM = $_POST['descriptionM'];
-                           $supM = $_POST['superficieM'];
-                           $addM = $_POST['adresseM'];
-                           $montantM = $_POST['montantM'];
-                           $dateM = $_POST['dateM'];
-                           $typeM = $_POST['typeM']; 
-
-                        if(isset($_POST['modifier'])){
-                        $IdMod =$_POST['indexMd'];
-                         $requeteModifier = "UPDATE `annonce` SET `TitreAnnonce` = '$titreM', `ImageAnnonce` = '$imgM', `DescriptionAnnonce` = '$descM', `AdresseAnnonce` = '$addM', `MontantAnnonce` = '$montantM', `DateAnnonce` = '$dateM', `TypeAnnonce` = '$typeM' WHERE `IdAnnonce` = $IdMod";
-                        $resultatRqModifier = $dbco->prepare($requeteModifier);        
-                        $resultatRqModifier ->execute();
-                        }
-                        ?> 
-                   
-               <!-- SuppressionModal -->
-                <div class='modal fade' id='SuppressionModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                    <div class='modal-dialog'>
-                        <div class='modal-content'>
-                        <div class='modal-header'>
-                            <h1 class='modal-title fs-5' id='exampleModalLabel'>Voulez-Vous Supprimer Cette Annonce ?</h1>
-                            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                        </div>
-                        <div class='modal-footer'>
-                            <form action="" method="POST">
-                            <input type='hidden' id='idinp'  name='indexS' value=''></input>
-                            <button type='submit' class='btn btn-danger' name='supprimer'>Confirmer</button>
-                            </form>
-                     <?php 
-                      if(isset($_POST["supprimer"])){
-                        $IdSup =$_POST['indexS'];
-                        $requeteSupprimer = "DELETE FROM annonce WHERE IdAnnonce = $IdSup";
-                        $resultatRqSupprimer = $dbco->prepare($requeteSupprimer);        
-                        $resultatRqSupprimer ->execute();
-                    }
-                    ?> 
-                    </div>
-
-                </div>  
+                
         </section>
     </main>
 
